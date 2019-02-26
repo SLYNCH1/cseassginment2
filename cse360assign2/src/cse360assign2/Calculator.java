91c8f7e1b8ee03da61e1d76b0a58d94f8723c2ac
Scott <scottflynch@gmail.com>
2019-02-25 18:37:55 -0700

    First version

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -11,30 +11,27 @@ public class Calculator {
 	// returns the value of total
 	// @return  the value of total 
 	public int getTotal () {
-		return total;
+		return 0;
 	}
 	// @param value - value added to total
 	// adds value to the total
 	public void add (int value) {
-		 total += value;;
 		
 	}
 	// @param value - value subtracted to total
 	// subtracts value to the total
 	public void subtract (int value) {
-		total -= value;
+		
 	}
 	// @param value - value multiplied by total
 		// multiplies value to the total
 	public void multiply (int value) {
-		total = value*total;
+		
 	}
 	// @param value - value which total is divided
 			// multiplies value to the total
 	public void divide (int value) {
-		if(value != 0)
-			total = total / value;
-			
+		
 	}
 	// @return the history of changes to total
 	// returns all changes to total
